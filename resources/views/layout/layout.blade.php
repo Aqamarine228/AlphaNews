f6fcbfa5f9b1a77efc75e07ef3566084e339382a
Aqamarine228 <MrFurgJK@gmail.com>
2022-08-02 15:08:20 +0200

    decomposed footer

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -40,12 +40,7 @@
         </section>
     </div>
 
-    <footer class="main-footer">
-        <div class="float-right d-none d-sm-block">
-            <strong>Version</strong> 3.2.0
-        </div>
-        <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
-    </footer>
+    @include('alphanews::layout._footer')
 </div>
 @include('alphanews::layout._scripts')
 </body>
